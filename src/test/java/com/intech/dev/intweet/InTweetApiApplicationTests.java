c01d24dbe549cf9e23ddf6113839ee65bf5d8f86
Liduen <75698473+Liduen26@users.noreply.github.com>
2024-12-05 10:24:40 +0100

    Initial commit

---
+++ src/test/java/com/intech/dev/intweet/InTweetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intech.dev.intweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTweetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
