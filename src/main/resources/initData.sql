cc320aa5c4d88b22ec44c4d62c618bed36c8bfac
Liduen <75698473+Liduen26@users.noreply.github.com>
2025-01-29 21:06:53 +0100

    Add trigger DB

--- src/main/resources/initData.sql
+++ src/main/resources/initData.sql
@@ -7,3 +7,32 @@ INSERT INTO testdb.posts (id, title, body, user_id, status, created_at, image, p
 INSERT INTO testdb.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (4, 'Test1', 'Lorem ipsum el dolot ...', 2, 'test', '2024-12-06 09:43:07', null, null);
 
 INSERT INTO testdb.likes (id, post_id, user_id) VALUES (1, 1, 1);
+
+DELIMITER //
+
+CREATE TRIGGER before_insert_posts
+    BEFORE INSERT ON posts
+    FOR EACH ROW
+BEGIN
+    IF CHAR_LENGTH(NEW.body) > 100 THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'DB Trigger : Le nombre de charactères du post ne doit pas dépasser 100 caractères';
+    END IF;
+END;
+//
+
+DELIMITER ;
+DELIMITER //
+
+CREATE TRIGGER before_update_posts
+    BEFORE UPDATE ON posts
+    FOR EACH ROW
+BEGIN
+    IF CHAR_LENGTH(NEW.body) > 100 THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'DB Trigger : Le nombre de charactères du post ne doit pas dépasser 100 caractères';
+    END IF;
+END;
+//
+
+DELIMITER ;
