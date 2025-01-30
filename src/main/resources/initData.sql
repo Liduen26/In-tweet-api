ebf2c9e3cdc43227d4d02da74b0fa7e440fc614f
Liduen <75698473+Liduen26@users.noreply.github.com>
2025-01-30 11:56:50 +0100

    Add new profiles and config files

--- src/main/resources/initData.sql
+++ src/main/resources/initData.sql
@@ -1,12 +1,12 @@
-INSERT INTO testdb.users (id, username, password, created_at, admin) VALUES (1, 'test', '$2y$10$NMQyUj0ZC5/y.hFSE/HlMuoq2oe9opnnRARTOEOzrfmnbGtj3dJ8m', '2024-12-06 09:43:41', 1);
-INSERT INTO testdb.users (id, username, password, created_at, admin) VALUES (2, 'jb', '$2y$10$NMQyUj0ZC5/y.hFSE/HlMuoq2oe9opnnRARTOEOzrfmnbGtj3dJ8m', '2024-12-06 09:43:41', 0);
+INSERT INTO intweet.users (id, username, password, created_at, admin) VALUES (1, 'test', '$2y$10$NMQyUj0ZC5/y.hFSE/HlMuoq2oe9opnnRARTOEOzrfmnbGtj3dJ8m', '2024-12-06 09:43:41', 1);
+INSERT INTO intweet.users (id, username, password, created_at, admin) VALUES (2, 'jb', '$2y$10$NMQyUj0ZC5/y.hFSE/HlMuoq2oe9opnnRARTOEOzrfmnbGtj3dJ8m', '2024-12-06 09:43:41', 0);
 
-INSERT INTO testdb.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (1, 'Test1', 'Lorem ipsum el dolot ...', 1, 'test', '2024-12-06 09:43:07', null, null);
-INSERT INTO testdb.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (2, 'Test1', 'Lorem ipsum el dolot ...', 2, 'test', '2024-12-06 09:43:07', null, null);
-INSERT INTO testdb.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (3, 'Test1', 'Lorem ipsum el dolot ...', 2, 'test', '2024-12-06 09:43:07', null, null);
-INSERT INTO testdb.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (4, 'Test1', 'Lorem ipsum el dolot ...', 2, 'test', '2024-12-06 09:43:07', null, null);
+INSERT INTO intweet.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (1, 'Test1', 'Lorem ipsum el dolot ...', 1, 'test', '2024-12-06 09:43:07', null, null);
+INSERT INTO intweet.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (2, 'Test1', 'Lorem ipsum el dolot ...', 2, 'test', '2024-12-06 09:43:07', null, null);
+INSERT INTO intweet.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (3, 'Test1', 'Lorem ipsum el dolot ...', 2, 'test', '2024-12-06 09:43:07', null, null);
+INSERT INTO intweet.posts (id, title, body, user_id, status, created_at, image, parent) VALUES (4, 'Test1', 'Lorem ipsum el dolot ...', 2, 'test', '2024-12-06 09:43:07', null, null);
 
-INSERT INTO testdb.likes (id, post_id, user_id) VALUES (1, 1, 1);
+INSERT INTO intweet.likes (id, post_id, user_id) VALUES (1, 1, 1);
 
 DELIMITER //
 
